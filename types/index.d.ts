8207b9bb2946729509c45ef00a3599391e8303d2
Venkat Sai Kedari Nath <venkatsaikedarinath@gmail.com>
2024-05-14 17:34:01 +0530

    addedtyechanges

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,12 +5,14 @@ declare type CreateUserParams = {
     clerkId: string;
     email: string;
     username: string;
+    firstName: string;
+    lastName: string;
     photo: string;
 };
 
 declare type UpdateUserParams = {
-    firstName: string | null;
-    lastName: string | null;
+    firstName: string;
+    lastName: string;
     username: string;
     photo: string;
 };
